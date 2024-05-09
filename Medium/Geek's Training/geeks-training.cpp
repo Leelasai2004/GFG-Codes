e08fc6a616bf19dabfb7cae0135eb402c3bfc42e
Leelasai2004 <157008562+Leelasai2004@users.noreply.github.com>
2024-05-09 16:17:12 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Geek's Training/geeks-training.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int helper(vector<vector<int>>& points,vector<vector<int>> &dp,int n,int task){
+        if(dp[n-1][task]!=-1){
+            return dp[n-1][task];
+        }
+        int maxi=INT_MIN;
+        for(int i=0;i<=2;i++){
+            if(task!=i){
+                maxi=max(maxi,points[n-1][i]+helper(points,dp,n-1,i));
+            }
+        }
+        return dp[n-1][task]=maxi;
+    }
+    int maximumPoints(vector<vector<int>>& points, int n) {
+        // Code here
+        vector<vector<int>> dp(n,vector<int>(4,-1));
+        dp[0][0]=max(points[0][1],points[0][2]);
+        dp[0][1]=max(points[0][2],points[0][0]);
+        dp[0][2]=max(points[0][0],points[0][1]);
+        dp[0][3]=max(dp[0][0],max(dp[0][1],dp[0][2]));
+        return helper(points,dp,n,3);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> points;
+        for (int i = 0; i < n; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            points.push_back(temp);
+        }
+
+        Solution obj;
+        cout << obj.maximumPoints(points, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
