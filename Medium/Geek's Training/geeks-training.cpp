9c7497f00bb29d7d69f7b03dedd136049310ef12
Leelasai2004 <157008562+Leelasai2004@users.noreply.github.com>
2024-05-09 20:15:54 +0530

    Updated Solution - GfG to GitHub

--- Medium/Geek's Training/geeks-training.cpp
+++ Medium/Geek's Training/geeks-training.cpp
@@ -24,7 +24,15 @@ class Solution {
         dp[0][1]=max(points[0][2],points[0][0]);
         dp[0][2]=max(points[0][0],points[0][1]);
         dp[0][3]=max(dp[0][0],max(dp[0][1],dp[0][2]));
-        return helper(points,dp,n,3);
+        // return helper(points,dp,n,3);
+        for(int i=1;i<n;i++){
+            for(int task=0;task<=2;task++){
+              dp[i][0]=max(points[i][1]+dp[i-1][1],points[i][2]+dp[i-1][2]);
+              dp[i][1]=max(points[i][0]+dp[i-1][0],points[i][2]+dp[i-1][2]);
+              dp[i][2]=max(points[i][0]+dp[i-1][0],points[i][1]+dp[i-1][1]);
+            }
+        }
+        return max(dp[n-1][0],max(dp[n-1][1],dp[n-1][2]));
     }
 };
 
