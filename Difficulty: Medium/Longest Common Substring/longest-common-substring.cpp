b1cf914570a6685e131f75649eb6c3af84f0ddb9
Leelasai2004 <157008562+Leelasai2004@users.noreply.github.com>
2024-06-25 19:08:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -12,17 +12,7 @@ class Solution{
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s[i-1]==t[j-1]){
-                    if(i==1 || j==1){
-                        dp[i][j]=1;
-                    }
-                    else{
-                        if(s[i-2]==t[j-2]){
-                            dp[i][j]=max(dp[i][j],dp[i-1][j-1]+1);
-                        }
-                        else{
-                            dp[i][j]=1;
-                        }
-                    }
+                    dp[i][j]=max(dp[i][j],1+dp[i-1][j-1]);
                 }
                
             }
